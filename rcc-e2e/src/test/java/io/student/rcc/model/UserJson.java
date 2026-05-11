3feff1dd20f0428c22d4d82c57fbcbc8f8f31426
sasoriq <vikotoed@gmail.com>
2026-05-11 18:49:36 +0700

    add extra json properties

--- rcc-e2e/src/test/java/io/student/rcc/model/UserJson.java
+++ rcc-e2e/src/test/java/io/student/rcc/model/UserJson.java
@@ -6,5 +6,7 @@ import java.util.UUID;
 
 public record UserJson(
     @JsonProperty("id") UUID id,
-    @JsonProperty("username") String username
+    @JsonProperty("username") String username,
+    @JsonProperty("firstname") String firstname,
+    @JsonProperty("avatar") String avatar
 ) {}
