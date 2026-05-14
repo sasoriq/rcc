ca2ec729ce72bae231f1fe0edc19790f3306b30c
sasoriq <vikotoed@gmail.com>
2026-05-14 23:30:47 +0700

    delete

--- rcc-api/src/main/java/io/student/rococo/model/ArtistJson.java
+++
@@ -1,9 +0,0 @@
-package io.student.rococo.model;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-import java.util.UUID;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record ArtistJson(UUID id, String name, String biography, String photo) {
-}
--- rcc-api/src/main/java/io/student/rococo/model/GeoJson.java
+++
@@ -1,7 +0,0 @@
-package io.student.rococo.model;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record GeoJson(String city, CountryJson country) {
-}
